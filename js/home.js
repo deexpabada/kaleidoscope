6c758ecd9494572d952459a1a8878d9bbabdcc60
talhaahsan <stwyde@gmail.com>
2016-02-25 11:54:52 -0600

    Added 3 hexagons with triangles inside, need to do math for other shapes. Looks pretty.

--- js/home.js
+++ js/home.js
@@ -3,37 +3,15 @@ $(function() {
     console.log(canvas)
     var g = canvas[0].getContext('2d')
 
-    //drawing lines
-    g.moveTo(10, 10)
-    g.lineTo(12, 40)
-    g.lineTo(21, 53)
-    g.stroke()
-
-    //draw mirroed line
-    g.translate(0, 100);
-    g.scale(1, -1);
-    g.moveTo(10, 10)
-    g.lineTo(12, 40)
-    g.lineTo(21, 53)
-    g.stroke()
 
     //draw CIRCLE
     //g.beginPath();
     //g.arc(95, 50, 40, 0, 2 * Math.PI);
     //g.stroke();
 
-    // set gradient value
-    var grd = g.createLinearGradient(100, 0, 200, 0);
-    grd.addColorStop(0, "green");
-    grd.addColorStop(1, "blue");
-
-    //draw gradient rectangle
-    g.fillStyle = grd;
-    //g.fillRect(80, 60, 75, 50);
-
     //show image and rotate the image
     var img = new Image();
-    img.src = "../images/k.jpg"
+    img.src = "../images/p.jpg"
 
     var img_x1 = img.height;
     var img_x2 = img.width;
@@ -42,42 +20,63 @@ $(function() {
     console.log(img_x2);
     console.log(img_x3);
 
-    function drawTriangle(){
-
-    }
 
     function findCordinate(){
 
     }
 
+    function draw(){
+        g.translate(300, 300);
+        g.save();
+        drawHex();
+        g.restore();
 
-    function draw() {
-        g.translate(0, 100);
-        g.scale(1, -1);
+        g.save()
+        g.translate(300, Math.sqrt(40000-10000));
+        drawHex();
+        g.restore();
+
+        g.translate(0, 2*Math.sqrt(40000-10000));
+        drawHex();
+        //g.rotate(45*Math.PI/180);
+
+    }
+
+    function drawHex(){
+        var symettry = 6;
+        for(i = 0; i< symettry; i++) {
+            if(i%2 == 0){
+                drawTriangle();
+            }
+            if(i%2 == 1){
+                g.save();
+                g.scale(-1,1);
+                drawTriangle();
+                g.restore()
+            }
+            g.rotate((60) * Math.PI / 180);
+
+        }
+    }
+    function drawTriangle() {
+
+        //g.translate(300,300);
+        //g.rotate((45*Math.PI/180));
+        //assuming image is 200x200, we want a triangle with a length of XYZ
+        //g.drawImage(img, 0, 0, 200, 200);
 
         g.save();
         g.beginPath();
-        g.rotate(0.3);
-        g.moveTo(50+231/2,50)
-        g.lineTo(50+215,50+235)
-        g.lineTo(50,50+231)
-        //g.translate(20,20);
+        g.moveTo(200,0);
+        g.lineTo(100, Math.sqrt(40000-10000));
+        g.lineTo(0, 0);
         g.clip();
-        //g.translate(30,0)
-        //g.fillStyle = "magenta";
-        //g.fillRect(-1000,-1000,2000,2000);
-        g.drawImage(img, 55, 50, 200, 200);
+        g.drawImage(img, 0, 0);
         g.restore();
-
-
-        //g.rotate(0.1)
-        //g.drawImage(img, 56, 50, 100, 150);
-        //g.rotate(0.2)
-        //g.drawImage(img, 150, 70, 40, 40);
     }
 
         //showing img rotating at different angles on the screen
-    img.onload = draw
+    img.onload = draw;
 
 });
 
