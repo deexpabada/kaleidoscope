bf07c6fe08e83a672cef1f603d703439535dccc3
Qinxi Wang <qwang@macalester.edu>
2016-02-24 19:47:21 -0600

    triangle clip

--- js/home.js
+++ js/home.js
@@ -9,20 +9,6 @@ $(function() {
     g.lineTo(21, 53)
     g.stroke()
 
-    //draw circle
-    g.beginPath();
-    g.arc(95, 50, 40, 0, 2 * Math.PI);
-    g.stroke();
-
-    // set gradient value
-    var grd = g.createLinearGradient(100, 0, 200, 0);
-    grd.addColorStop(0, "green");
-    grd.addColorStop(1, "blue");
-
-    //draw gradient rectangle
-    g.fillStyle = grd;
-    g.fillRect(80, 60, 75, 50);
-
     //draw mirroed line
     g.translate(0, 100);
     g.scale(1, -1);
@@ -31,22 +17,67 @@ $(function() {
     g.lineTo(21, 53)
     g.stroke()
 
+    //draw CIRCLE
+    //g.beginPath();
+    //g.arc(95, 50, 40, 0, 2 * Math.PI);
+    //g.stroke();
+
+    // set gradient value
+    var grd = g.createLinearGradient(100, 0, 200, 0);
+    grd.addColorStop(0, "green");
+    grd.addColorStop(1, "blue");
+
+    //draw gradient rectangle
+    g.fillStyle = grd;
+    //g.fillRect(80, 60, 75, 50);
 
     //show image and rotate the image
     var img = new Image();
     img.src = "../images/k.jpg"
-    img.onload = function() {
-        //console.log(img.src)
+
+    var img_x1 = img.height;
+    var img_x2 = img.width;
+    var img_x3 = canvas.centerline;
+    console.log(img_x1);
+    console.log(img_x2);
+    console.log(img_x3);
+
+    function drawTriangle(){
+
+    }
+
+    function findCordinate(){
+
+    }
+
+
+    function draw() {
         g.translate(0, 100);
         g.scale(1, -1);
 
-        g.rotate(0.5)
-        g.drawImage(img, 120, 50, 50, 50);
-        g.rotate(0.1)
-        g.drawImage(img, 260, 10, 50, 50);
-        g.rotate(0.2)
-        g.drawImage(img, 150, 70, 40, 40);
+        g.save();
+        g.beginPath();
+        g.rotate(0.3);
+        g.moveTo(50+231/2,50)
+        g.lineTo(50+215,50+235)
+        g.lineTo(50,50+231)
+        //g.translate(20,20);
+        g.clip();
+        //g.translate(30,0)
+        //g.fillStyle = "magenta";
+        //g.fillRect(-1000,-1000,2000,2000);
+        g.drawImage(img, 55, 50, 200, 200);
+        g.restore();
+
+
+        //g.rotate(0.1)
+        //g.drawImage(img, 56, 50, 100, 150);
+        //g.rotate(0.2)
+        //g.drawImage(img, 150, 70, 40, 40);
     }
+
+        //showing img rotating at different angles on the screen
+    img.onload = draw
 
 });
 
