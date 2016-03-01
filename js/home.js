d8cb29607606e052a3ce0e6d508c7056d1be9094
Qinxi Wang <qwang@macalester.edu>
2016-02-29 21:41:27 -0600

    add a new method that clip a huge circle on the center

--- js/home.js
+++ js/home.js
@@ -3,7 +3,6 @@ $(function() {
     console.log(canvas);
     var g = canvas[0].getContext('2d');
 
-
     //draw CIRCLE
     //g.beginPath();
     //g.arc(95, 50, 40, 0, 2 * Math.PI);
@@ -11,21 +10,9 @@ $(function() {
 
     //show image and rotate the image
     var img = new Image();
-    img.src = "../images/PaulAlt.jpg";
+    img.src = "../images/k.jpg";
 
-    var img_x1 = img.height;
-    var img_x2 = img.width;
-    var img_x3 = canvas.centerline;
-    console.log(img_x1);
-    console.log(img_x2);
-    console.log(img_x3);
-
-
-    function findCordinate(){
-
-    }
-
-    function draw(){
+    function drawMultipleHexs(){
         g.translate(300, 300);
         g.save();
         drawHex();
@@ -36,45 +23,72 @@ $(function() {
         drawHex();
         g.restore();
 
+        g.save();
         g.translate(0, 2*Math.sqrt(40000-10000));
         drawHex();
-        //g.rotate(45*Math.PI/180);
+        g.restore();
 
+        g.save();
+        g.translate(600, 2*Math.sqrt(40000-10000));
+        drawHex();
+        g.restore();
+
+        g.save();
+        g.translate(600, 1/200*Math.sqrt(40000-10000));
+        drawHex();
+        g.restore();
+
+        g.save();
+        g.translate(300, 3*Math.sqrt(40000-10000));
+        drawHex();
+        g.restore();
+
+        g.save();
+        g.translate(300, -1*Math.sqrt(40000-10000));
+        drawHex();
+        g.restore();
     }
 
     function drawHex(){
-        var symettry = 8;
-        for(i = 0; i< symettry; i++) {
+        for(i = 0; i< 6; i++) {
             if(i%2 == 0){
                 drawTriangle();
             }
             if(i%2 == 1){
                 g.save();
-                g.rotate(45*Math.PI/180);
                 g.scale(-1,1);
                 drawTriangle();
                 g.restore()
             }
-            g.rotate(45 * Math.PI / 180);
+            g.rotate(60 * Math.PI / 180);
 
         }
     }
     function drawTriangle() {
-
-        //g.translate(300,300);
-        //g.rotate((45*Math.PI/180));
         //assuming image is 200x200, we want a triangle with a length of XYZ
         //g.drawImage(img, 0, 0, 200, 200);
 
         g.save();
         g.beginPath();
         g.moveTo(200,0);
-        //g.lineTo(100, Math.sqrt(40000-10000));
-        g.arc(0, 0, Math.sqrt(30000), 0,.25*Math.PI);
+        g.lineTo(100, Math.sqrt(40000-10000));
+        //g.arc(0, 0, Math.sqrt(30000), 0,.25*Math.PI);
         g.lineTo(0, 0);
         g.clip();
         g.drawImage(img, 0, 0);
         g.restore();
+    }
+
+    function draw(){
+
+        g.save();
+        g.beginPath();
+        g.arc(600, 300, 300, 0, 2 * Math.PI);
+        g.clip();
+        drawMultipleHexs();
+        g.restore();
+
+
     }
 
         //showing img rotating at different angles on the screen
